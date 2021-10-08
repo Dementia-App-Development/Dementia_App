befced181a265c7b0d1e0cb484b4fe68985f3d57
Orange Man <303221459@qq.com>
2021-10-08 22:30:25 +1100

    add drawer

--- DementiaApp/app/src/main/java/com/dementiaapp/quiz/MainActivity.kt
+++ DementiaApp/app/src/main/java/com/dementiaapp/quiz/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.dementiaapp.quiz.databinding.ActivityMainBinding
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
 
     // Creating a binding object for the main_activity.xml layout
     private lateinit var binding: ActivityMainBinding
+
+    // create a reference to drawerLayout
+    private lateinit var drawerLayout: DrawerLayout
+
 
     // Initialize the current question index to 0 (the first question)
     private var currentQuestionIndex = 0
@@ -26,15 +31,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // get drawerLayout
+        drawerLayout = binding.drawerLayout
+
+
+        // connect navController with actionBar
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+
+        // display the navigation drawer
+        NavigationUI.setupWithNavController(binding.navView, navController)
 
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 
 }
