a2e5625747cfb432fbee9ae6b998d7d1a0f1d8dd
Aleksandar Pasquini <aleks.pasquini@gmail.com>
2021-10-20 21:17:33 +1100

    More UI changes

--- DementiaApp/app/src/main/java/com/dementiaquiz/android/fragments/QuizFragment.kt
+++ DementiaApp/app/src/main/java/com/dementiaquiz/android/fragments/QuizFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +20,12 @@ import com.dementiaquiz.android.models.QuizViewModel
 import timber.log.Timber
 import java.util.*
 import android.os.CountDownTimer as CountDownTimer
+import android.graphics.drawable.Drawable
+import android.opengl.Visibility
+import android.util.Log
+import java.io.InputStream
+import java.net.URL
+
 
 /**
  * Displays the current quiz question and handles user input in answering the question
@@ -26,8 +33,9 @@ import android.os.CountDownTimer as CountDownTimer
 class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
 
     private lateinit var viewModel: QuizViewModel
-    private val REQUEST_CODE_SPEECH_INPUT = 100;
+    private val REQUEST_CODE_SPEECH_INPUT = 100
     private var tts: TextToSpeech? = null
+    private var answer: String? = null
     private lateinit var binding: FragmentQuizBinding
 
     /**
@@ -35,11 +43,10 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
      */
     override fun onInit(status: Int) {
         if(status != TextToSpeech.ERROR) {
-            tts?.language = Locale.UK;
-            tts!!.speak("What year is this?", TextToSpeech.QUEUE_FLUSH, null)
+            tts?.language = Locale.UK
         }
     }
-    public override fun onDestroy() {
+    override fun onDestroy() {
         // Shutdown TTS
         if (tts != null) {
             tts!!.stop()
@@ -55,9 +62,12 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
                     // Get Text from result
                     val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
 
-
+                    val voice = result?.get(0)
                     // Return result of question
-                    //if (textViewVoiceAnswer.text == "no ifs ands or buts") {
+                    Timber.i(voice)
+                    if (voice contentEquals answer) {
+                        Timber.i("Well Done")
+                    }
                     //    textViewAnswerResult.text = "Correct"
                     //} else {
                     //    textViewAnswerResult.text = "Incorrect"
@@ -86,7 +96,19 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
         var CountDown: CountDownTimer? = null
         // Set up LiveData observation relationships
         viewModel.currentQuestion.observe(viewLifecycleOwner, Observer<QuizQuestion> { newQuestion ->
-
+            Timber.i(newQuestion.answers.toString() )
+            binding.tvInstructions.visibility = View.VISIBLE
+            binding.trueButton.visibility = View.GONE
+            binding.falseButton.visibility = View.GONE
+            if (TextUtils.isEmpty(newQuestion.image_url)){
+                binding.tvSubText.visibility  = View.GONE
+                binding.imageView.visibility = View.VISIBLE
+                binding.imageView.setImageDrawable(LoadImageFromWebOperations(newQuestion.image_url))
+            }
+            else {
+                binding.tvSubText.visibility  = View.VISIBLE
+                binding.imageView.visibility = View.GONE
+            }
             if (newQuestion.response_type == QuizQuestion.ResponseType.DATE) {
                 binding.Text.visibility = View.GONE
                 binding.date.visibility = View.VISIBLE
@@ -106,6 +128,7 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
                 binding.Text.visibility = View.GONE
                 binding.date.visibility = View.GONE
                 binding.button.visibility = View.GONE
+                binding.tvInstructions.visibility = View.INVISIBLE
                 binding.voiceButton.visibility = View.VISIBLE
                 binding.nextBtn.visibility = View.GONE
                 CountDown = viewModel.startTimer(binding, 60 * 1000)
@@ -121,19 +144,27 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
                     newQuestion.sub_question.toString()
             binding.tvInstructions.text = newQuestion.instruction
             binding.tvSubText.text = newQuestion.sub_text
+            answer = newQuestion.answers.toString()
             tts!!.speak(newQuestion.instruction, TextToSpeech.QUEUE_FLUSH, null)
-
         })
 
         binding.button.setOnClickListener{
             binding.nextBtn.visibility = View.VISIBLE
             binding.bar.visibility = View.VISIBLE
             binding.button.visibility = View.GONE
-            binding.Text.visibility = View.VISIBLE
+            binding.trueButton.visibility = View.VISIBLE
+            binding.falseButton.visibility = View.VISIBLE
             CountDown = viewModel.startTimer(binding, 60 * 1000)
         }
         // Go to the next question
         binding.nextBtn.setOnClickListener {
+            if (binding.Text.visibility == View.VISIBLE){
+                var string = binding.Text.text.toString()
+                if (answer?.contains(string) == true){
+                    Timber.i("Well Done")
+                    // If string == answer do something
+                }
+            }
             CountDown?.cancel()
             viewModel.onNext()
         }
@@ -154,7 +185,7 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
             }
         }
         binding.voiceButton.setOnClickListener{
-            speak();
+            speak()
             binding.nextBtn.visibility = View.VISIBLE
         }
 
@@ -165,5 +196,12 @@ class QuizFragment : Fragment(), TextToSpeech.OnInitListener {
 
         return binding.root
     }
-
+    fun LoadImageFromWebOperations(url: String?): Drawable? {
+        return try {
+            val `is`: InputStream = URL(url).content as InputStream
+            Drawable.createFromStream(`is`, "src name")
+        } catch (e: java.lang.Exception) {
+            null
+        }
+    }
 }
