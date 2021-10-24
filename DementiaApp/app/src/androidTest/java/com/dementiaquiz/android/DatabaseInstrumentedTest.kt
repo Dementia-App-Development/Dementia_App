a559c28863ff6be8a52e4a7548b7bb2cb072a1df
Orange Man <303221459@qq.com>
2021-10-24 23:16:55 +1100

    update tests

--- DementiaApp/app/src/androidTest/java/com/dementiaquiz/android/DatabaseInstrumentedTest.kt
+++ DementiaApp/app/src/androidTest/java/com/dementiaquiz/android/DatabaseInstrumentedTest.kt
@@ -10,6 +10,8 @@ import com.dementiaquiz.android.database.QuizDatabase
 import com.dementiaquiz.android.database.dao.QuizAnswerDao
 import com.dementiaquiz.android.database.dao.QuizResultDao
 import com.dementiaquiz.android.database.dao.UserDao
+import com.dementiaquiz.android.database.model.QuizAnswer
+import com.dementiaquiz.android.database.model.QuizResult
 import com.dementiaquiz.android.database.model.User
 import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
@@ -66,6 +68,58 @@ class DatabaseInstrumentedTest {
         val result = userDao.getUsers()
 
         assertThat(result.size, equalTo(2))
-        
+
+    }
+
+    @Test
+    fun getQuizResultsByUserId(){
+
+        val user1: User = User(10, "Rachel", "Wilcher",convertStringToDate("2021/10/24"))
+        val user2: User = User(12, "Stephen", "Cox",convertStringToDate("2021/10/24"))
+
+        userDao.insert(user1)
+        userDao.insert(user2)
+
+        val quizResult1:QuizResult = QuizResult(1,10,80,convertStringToDate("2021/10/24"))
+        val quizResult2:QuizResult = QuizResult(2,12,50,convertStringToDate("2021/10/24"))
+
+        quizResultDao.insert(quizResult1)
+        quizResultDao.insert(quizResult2)
+
+        val result = quizResultDao.getQuizResultsByUserId(10)
+//        println(result)
+        assertThat(result.get(0), equalTo(quizResult1))
+
+    }
+
+    @Test
+    fun getQuizAnswersByResultId(){
+        val quizResult1:QuizResult = QuizResult(1,10,80,convertStringToDate("2021/10/24"))
+        val quizResult2:QuizResult = QuizResult(2,12,50,convertStringToDate("2021/10/24"))
+
+        quizResultDao.insert(quizResult1)
+        quizResultDao.insert(quizResult2)
+
+        val quizAnswer1:QuizAnswer= QuizAnswer(0,"Which city are you in now?","Melbourne","Sydney",false,1)
+        val quizAnswer2:QuizAnswer=QuizAnswer(0,"How old are you?","55","55",true,1)
+        val quizAnswer3:QuizAnswer=QuizAnswer(0,"Which color is it?","Blue","Yellow",false,2)
+
+        quizAnswerDao.insert(quizAnswer1)
+        quizAnswerDao.insert(quizAnswer2)
+        quizAnswerDao.insert(quizAnswer3)
+
+        val result=quizAnswerDao.getQuizAnswersByResultId(1)
+//        println(result)
+        assertThat(result.size, equalTo(2))
+
+    }
+
+
+
+    // the parameter "dateStr" should be in the format of "yyyy/mm/dd"
+    fun convertStringToDate(dateStr:String):Date{
+        val sdf = SimpleDateFormat("yyyy/MM/dd")
+        val date: Date = sdf.parse(dateStr)
+        return date
     }
 }
