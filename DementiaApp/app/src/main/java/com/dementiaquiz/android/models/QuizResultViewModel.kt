16f15961f7ef0900816d52e628981923badbf45c
Orange Man <303221459@qq.com>
2021-10-27 16:29:18 +1100

    add QuizResultViewModel and update dependency

---
+++ DementiaApp/app/src/main/java/com/dementiaquiz/android/models/QuizResultViewModel.kt
@@ -0,0 +1,41 @@
+package com.dementiaquiz.android.models
+
+import androidx.lifecycle.*
+import com.dementiaquiz.android.database.model.QuizResult
+import com.dementiaquiz.android.repositories.QuizResultRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class QuizResultViewModel(private val quizResultRepository: QuizResultRepository):ViewModel(){
+
+    /**
+     * Cite from "https://developer.android.com/codelabs/android-room-with-a-view-kotlin#9"
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(quizResult: QuizResult) = viewModelScope.launch {
+
+        quizResultRepository.insert(quizResult)
+    }
+
+    // The comment below is quoted from "https://developer.android.com/codelabs/android-room-with-a-view-kotlin#9"
+    // Using LiveData and caching have several benefits:
+    // - We can put an observer on the data (instead of polling for changes) and only update the
+    //   the UI when the data actually changes.
+    // - Repository is completely separated from the UI through the ViewModel.
+    fun getQuizResults():LiveData<List<QuizResult>>{
+        return quizResultRepository.getQuizResults().asLiveData()
+    }
+
+}
+
+class QuizResultViewModelFactory(private val quizResultRepository: QuizResultRepository):ViewModelProvider.Factory{
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(QuizResultViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return QuizResultViewModel(quizResultRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
+}
