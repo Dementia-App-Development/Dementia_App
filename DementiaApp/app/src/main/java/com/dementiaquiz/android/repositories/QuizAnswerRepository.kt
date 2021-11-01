74033ede4b8f045ccc099a2db97b9015ff7d42da
Orange Man <303221459@qq.com>
2021-11-01 16:19:59 +1100

    Create QuizAnswerRepository.kt

---
+++ DementiaApp/app/src/main/java/com/dementiaquiz/android/repositories/QuizAnswerRepository.kt
@@ -0,0 +1,20 @@
+package com.dementiaquiz.android.repositories
+
+import androidx.annotation.WorkerThread
+import com.dementiaquiz.android.database.dao.QuizAnswerDao
+import com.dementiaquiz.android.database.model.QuizAnswer
+import com.dementiaquiz.android.database.model.QuizResult
+import kotlinx.coroutines.flow.Flow
+
+class QuizAnswerRepository(private val quizAnswerDao: QuizAnswerDao) {
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(quizAnswer: QuizAnswer){
+        quizAnswerDao.insert(quizAnswer)
+    }
+
+    fun getQuizAnswersByResultId(resultId:Long):Flow<List<QuizAnswer>> {
+        return quizAnswerDao.getQuizAnswersByResultId(resultId)
+    }
+}
