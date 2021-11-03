15eec2dece927d0463363dfdf4382587503e34cf
Orange Man <303221459@qq.com>
2021-11-03 12:40:28 +1100

    update recycle view (on progress)

--- DementiaApp/app/src/main/java/com/dementiaquiz/android/fragments/ResultsFragment.kt
+++ DementiaApp/app/src/main/java/com/dementiaquiz/android/fragments/ResultsFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dementiaquiz.android.R
+import com.dementiaquiz.android.databinding.FragmentResultsBinding
+import com.dementiaquiz.android.fragments.adapters.UserListAdapter
 import timber.log.Timber
 
 /**
  * Displays the historical data obtained from results from the quiz
  */
-class resultsFragment : Fragment() {
+class ResultsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,9 +24,27 @@ class resultsFragment : Fragment() {
 
         Timber.i("onCreateView called")
 
-        // Inflate the layout for this fragment
+        // Inflate view binding to get variables from XML
+        val binding = DataBindingUtil.inflate<FragmentResultsBinding>(
+            inflater,
+            R.layout.fragment_results,
+            container,
+            false
+        )
+
+
+        // set up the recycleView
+        val userRecyclerView = binding.userRecyclerview
+        val adapter = UserListAdapter()
+        userRecyclerView.adapter = adapter
+        userRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        Timber.i("recycleView adapter attached")
+
+
         // TODO change to nav host controller binding
-        return inflater.inflate(R.layout.fragment_results, container, false)
+
+        return binding.root
     }
 
 }
