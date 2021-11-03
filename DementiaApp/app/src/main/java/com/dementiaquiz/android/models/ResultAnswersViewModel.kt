5ca41d6fa44579be4bd45883ed8ad9441a8f1cbf
Orange Man <303221459@qq.com>
2021-11-03 21:50:15 +1100

    Update ResultAnswersViewModel.kt

--- DementiaApp/app/src/main/java/com/dementiaquiz/android/models/ResultAnswersViewModel.kt
+++ DementiaApp/app/src/main/java/com/dementiaquiz/android/models/ResultAnswersViewModel.kt
@@ -1,4 +1,33 @@
 package com.dementiaquiz.android.models
 
-class ResultAnswersViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import com.dementiaquiz.android.database.model.QuizAnswer
+import com.dementiaquiz.android.database.model.QuizResult
+import com.dementiaquiz.android.repositories.QuizAnswerRepository
+import com.dementiaquiz.android.repositories.QuizResultRepository
+
+class ResultAnswersViewModel(private val quizAnswerRepository: QuizAnswerRepository): ViewModel() {
+
+    // get the quiz answers belong to the given result ID
+    fun getQuizAnswersByResultId(resultId:Long): LiveData<List<QuizAnswer>> {
+        return quizAnswerRepository.getQuizAnswersByResultId(resultId).asLiveData()
+    }
+
+
+}
+
+class ResultAnswersViewModelFactory(private val quizAnswerRepository: QuizAnswerRepository):
+    ViewModelProvider.Factory{
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(ResultAnswersViewModel::class.java)){
+            @Suppress("UNCHECKED_CAST")
+            return ResultAnswersViewModel(quizAnswerRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+
 }
